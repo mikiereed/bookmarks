9ffe4db3992425d16891e87e7f58279ceff1a7f9
mikie Reed <mikie@prenotions.com>
2020-08-18 11:31:24 -0700

    login and logout views

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Login, logout and redirect
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
