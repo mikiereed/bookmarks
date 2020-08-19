28d8f819ee76d70a33dc9c8b2839e983bb33a39b
mikie Reed <mikie@prenotions.com>
2020-08-19 11:08:28 -0700

    start success or error messages

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from .forms import LoginForm, UserRegistrationForm,\
     UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
@@ -71,6 +72,10 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated '
+                                      'successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(
